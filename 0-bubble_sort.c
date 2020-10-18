67de75b4e258b83608e5592b480e58420600ba60
julianamonr03 <julianamonroy03@hotmail.com>
2020-10-18 18:57:12 +0000

    Bubble sort

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -2,10 +2,30 @@
 /**
  * bubble_sort - sorts an array of integers in ascending orde
  * @array: array of integers
- * @size: size of the array 
- * 
+ * @size: size of the array
  * Return - void
  */
 void bubble_sort(int *array, size_t size)
 {
+	size_t i, j;
+	int tmp; /* Temporal variable for swap */
+
+	if (size < 2 || array == NULL)
+	{
+		return;
+	}
+
+	for (i = 0; i < size - 1; i++)
+	{
+		for (j = 0; j < size - i - 1; j++)
+		{
+			if (array[j] > array[j + 1])
+			{
+				tmp = array[j];
+				array[j] = array[j + 1];
+				array[j + 1] = tmp;
+				print_array(array, size);
+			}
+		}
+	}
 }
