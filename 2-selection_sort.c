a88964d284b344876f0e66f7edcb088f2613986c
julianamonr03 <julianamonroy03@hotmail.com>
2020-10-19 01:55:08 +0000

    Fix [2]

--- 2-selection_sort.c
+++ 2-selection_sort.c
@@ -12,7 +12,7 @@ void selection_sort(int *array, size_t size)
 	if (size < 2 || array == NULL)
 		return;
 
-	for (i = 0; i < size - 1; i++)
+	for (i = 0; i < size; i++)
 	{
 		minimum = i;
 		for (j = i + 1; j < size; j++)
