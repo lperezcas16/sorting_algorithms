7944c2350c55a1a6a2706698652dbc577811dc19
julianamonr03 <julianamonroy03@hotmail.com>
2020-10-20 00:39:14 +0000

    Update header files

--- sort.h
+++ sort.h
@@ -25,4 +25,8 @@ void bubble_sort(int *array, size_t size);
 void selection_sort(int *array, size_t size);
 void insertion_sort_list(listint_t **list);
 void quick_sort(int *array, size_t size);
+void recursive(int *array, int first, int last, size_t size);
+int partition(int *array, int first, int last, int size);
+void swap(int *array, int i, int j, int size);
+
 #endif /* __SORT_ALG__ */
